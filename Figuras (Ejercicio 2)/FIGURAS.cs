3be705206f82df6e8dd9f31dd816d605cde04463
Sofía <sofiacm17@hotmail.com>
2021-02-15 18:43:07 +0100

    Agregar archivos de proyecto.

---
+++ Figuras (Ejercicio 2)/FIGURAS.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Figuras__Ejercicio_2_
+{
+    class FIGURAS
+    {
+        public decimal area { get; set; } //get y set son de tipo lectura, sintaxis para declarar un atributo
+        decimal lado;
+        decimal ancho;
+        decimal radio;
+
+        public void areaCuadrado()
+        {
+            Console.WriteLine("Añade el lado");
+            lado = Convert.ToDecimal(Console.ReadLine());
+            area = lado * lado;
+            Console.WriteLine("el resultado del area es:" + area);
+
+             
+        }
+        
+    }
+}
---
+++ Figuras (Ejercicio 2)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Figuras__Ejercicio_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FIGURAS Figuras = new FIGURAS(); //instanciando a la clase figuras
+            Figuras.areaCuadrado(); //accedemos al método creado
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
